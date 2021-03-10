0c851aa1fa6e11c0f4e07e3b83af7d111f67e82b
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-10 16:03:23 +0530

    rendering fetched data to the screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,37 +1,43 @@
- import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
 
-    componentDidMount() {
+    state = {
 
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(resonse => {
-            console.log(resonse)
-        })
+        posts: []
     }
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      this.setState({posts:response.data})
+    });
+  }
+  render() {
+
+    const posts = this.state.posts.map(post => {
+
+        return(
+            <Post key = {post.id} title = {post.title}/>
+        )
+    })
+    return (
+      <div>
+        <section className="Posts">
+         {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
