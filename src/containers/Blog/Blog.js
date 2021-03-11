c5a0be7158b5243f56efa6c199cbc0fd7084d4f1
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-11 16:01:25 +0530

    making a post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ class Blog extends Component {
 
     state = {
 
-        posts: []
+        posts: [],
+        selectedPostId:null
     }
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
@@ -27,12 +28,25 @@ class Blog extends Component {
       this.setState({posts:updatedPosts})
     });
   }
+
+  postSelectedHandler = (id) => {
+
+    this.setState({
+        selectedPostId:id
+    })
+
+
+}
   render() {
 
     const posts = this.state.posts.map(post => {
 
         return(
-            <Post key = {post.id} title = {post.title} author={post.author}/>
+            <Post 
+            key = {post.id} 
+            title = {post.title} 
+            author={post.author} 
+            clicked ={()=>this.postSelectedHandler(post.id)}/>
         )
     })
     return (
@@ -41,7 +55,7 @@ class Blog extends Component {
          {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id = {this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
