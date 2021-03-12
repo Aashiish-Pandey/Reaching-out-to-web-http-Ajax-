e9f80886a394c78a1bda2ae4527ded25af8577ed
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-12 09:12:28 +0530

    adding interceptors to execute code dynamically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Blog from './containers/Blog/Blog';
+
+axios.interceptors.request.use(request => {
+
+  console.log(request)
+  return request
+},error =>{
+  console.log(error)
+  return Promise.reject(error)
+})
+axios.interceptors.response.use(response => {
+
+  console.log(response)
+  return response
+},error =>{
+  console.log(error)
+  return Promise.reject(error)
+})
+
 
 class App extends Component {
   render() {
